4768f6da058b527d966db330001976eb46c17eaf
Cu-Rent <gray.xavier@mail.ru>
2023-06-12 20:03:47 +0300

    #7 - Добавил таблицу poll

---
+++ src/main/resources/db/migration/V1.0.0_3__Create_poll_table.sql
@@ -0,0 +1,12 @@
+-- Table: public.poll
+CREATE TYPE public.poll_enum AS ENUM ('SINGLE', 'VARIABLE');
+
+CREATE TABLE IF NOT EXISTS public."poll"
+(
+    id bigserial primary key,
+    survey_id bigint,
+    question_text character varying(100) NOT NULL,
+    poll_type poll_enum NOT NULL,
+    FOREIGN KEY (survey_id) REFERENCES public."survey" (id) ON DELETE CASCADE
+);
+
