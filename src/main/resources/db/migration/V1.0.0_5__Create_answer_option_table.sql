e2a4bcd2dcc2835d24add65947cfc15aecab8a3d
Nikita Kozel <niknikkzel@gmail.com>
2023-06-14 17:58:20 +0300

    #9 add in answer_option col "option_image"

--- src/main/resources/db/migration/V1.0.0_5__Create_answer_option_table.sql
+++ src/main/resources/db/migration/V1.0.0_5__Create_answer_option_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS public."answer_option"
 (
     id bigserial primary key,
     poll_id bigint REFERENCES public."poll" (id) ON DELETE CASCADE,
+    option_image BLOB,
     option_text TEXT NOT NULL,
     voted_count integer NOT NULL,
     type public.answer_type NOT NULL
