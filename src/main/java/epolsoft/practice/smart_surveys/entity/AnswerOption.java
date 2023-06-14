ecac4f4ceae8375f177b297a55df5dcf6a4779b3
Cu-Rent <gray.xavier@mail.ru>
2023-06-14 16:08:35 +0300

    #22 - Создал entity для AnswerOption и связал в poll

--- src/main/java/epolsoft/practice/smart_surveys/entity/AnswerOption.java
+++ src/main/java/epolsoft/practice/smart_surveys/entity/AnswerOption.java
@@ -1,5 +1,8 @@
 package epolsoft.practice.smart_surveys.entity;
+import epolsoft.practice.smart_surveys.enums.RoleType;
 import jakarta.persistence.*;
+import org.hibernate.annotations.Formula;
+import epolsoft.practice.smart_surveys.enums.AnswerType;
 
 public class AnswerOption
 {
@@ -9,8 +12,14 @@ public class AnswerOption
     @ManyToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name = "poll_id", referencedColumnName = "id")
     private Poll poll;
+    @Column(name = "option_image", nullable = false, columnDefinition = "BLOB")
+    private String image;
     @Column(name = "option_text", nullable = false, columnDefinition = "text")
     private String option;
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AnswerType type;
     @Column(name = "voted_count", nullable = false, columnDefinition = "integer default 0")
+    @Formula("(SELECT COUNT(id) FROM user_vote WHERE answer_option_id=id)")
     private int voted_count;
 }
