3989cc67ebe7bbddb4e24861971137549c407d07
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-14 13:30:08 +0300

    #6 - Changed the size for email and password columns to 50 and 30 chars respectfully

--- src/main/resources/db/migration/V1.0.0_1__Create_users_table.sql
+++ src/main/resources/db/migration/V1.0.0_1__Create_users_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS public."users"
 (
     id bigserial primary key,
     name varchar(50) NOT NULL,
-    email varchar(320) NOT NULL,
-    password text NOT NULL,
+    email varchar(50) NOT NULL,
+    password varchar(30) NOT NULL,
     role_type public.role_enum NOT NULL
 );
