5e844a36b48676ed29a7b4d21fd547989ebaa016
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-14 22:41:16 +0300

    #26 - Created service for Users and its yet-empty implementation class

---
+++ src/main/java/epolsoft/practice/smart_surveys/services/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package epolsoft.practice.smart_surveys.services.impl;
+
+import epolsoft.practice.smart_surveys.entity.User;
+import epolsoft.practice.smart_surveys.services.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void createUser(User user) {}
+
+    @Override
+    public void getUserById(Long id) {}
+
+    @Override
+    public void getAllUsers() {}
+
+    @Override
+    public void updateUser(User user, Long id) {}
+
+    @Override
+    public void deleteUser(Long id) {}
+}
