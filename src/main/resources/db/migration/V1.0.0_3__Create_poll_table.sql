ef3d081e69d76940137aaeb8ef1efd0db59d0c4e
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-15 11:24:41 +0300

    #7 - changed the type of column 'survey_description_image' from BLOB to BYTEA (PostgreSQL doesn't work with the former)

--- src/main/resources/db/migration/V1.0.0_3__Create_poll_table.sql
+++ src/main/resources/db/migration/V1.0.0_3__Create_poll_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS public."poll"
 (
     id bigserial primary key,
     survey_id bigint REFERENCES public."survey" (id) ON DELETE CASCADE,
-    poll_image BLOB,
+    poll_image bytea,
     question_text varchar(200) NOT NULL,
     poll_type poll_enum NOT NULL
 );
