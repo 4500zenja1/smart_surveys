1c4fb2cce252ec6bc27ef8461b441e28edb48cf7
Cu-Rent <gray.xavier@mail.ru>
2023-06-16 21:13:44 +0300

    #32 - Добавил mapper для poll и новое поле для PollRequestDto

--- src/main/java/epolsoft/practice/smart_surveys/dto/PollRequestDto.java
+++ src/main/java/epolsoft/practice/smart_surveys/dto/PollRequestDto.java
@@ -1,10 +1,13 @@
 package epolsoft.practice.smart_surveys.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import epolsoft.practice.smart_surveys.entity.AnswerOption;
 import epolsoft.practice.smart_surveys.entity.enums.PollType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -20,4 +23,6 @@ public class PollRequestDto {
     private String question;
     @JsonProperty("type")
     private PollType type;
+    @JsonProperty("answers")
+    private List<AnswerOption> answers;
 }
---
+++ src/main/java/epolsoft/practice/smart_surveys/mapper/PollMapper.java
@@ -0,0 +1,18 @@
+package epolsoft.practice.smart_surveys.mapper;
+
+import epolsoft.practice.smart_surveys.dto.AnswerOptionDto;
+import epolsoft.practice.smart_surveys.dto.PollRequestDto;
+import epolsoft.practice.smart_surveys.entity.AnswerOption;
+import epolsoft.practice.smart_surveys.entity.Poll;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PollMapper {
+    AnswerOptionDto answerToAnswerOptionDto(AnswerOption answerOption);
+
+    PollRequestDto pollToPollRequestDto(Poll poll);
+
+    List<PollRequestDto> pollsToPollRequestDto(List<Poll> polls);
+}
