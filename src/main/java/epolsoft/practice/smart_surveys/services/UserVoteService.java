832c8f8832d07be00aa61f65d122d118fc1b075f
Cu-Rent <gray.xavier@mail.ru>
2023-06-19 17:09:59 +0300

    #32 - удалил лишнюю функцию

--- src/main/java/epolsoft/practice/smart_surveys/services/UserVoteService.java
+++ src/main/java/epolsoft/practice/smart_surveys/services/UserVoteService.java
@@ -5,5 +5,4 @@ import epolsoft.practice.smart_surveys.entity.UserVote;
 
 public interface UserVoteService {
     public UserVote createUserVote(UserVote userVote);
-    public UserVote getUserVote(Long userVoteId);
 }
