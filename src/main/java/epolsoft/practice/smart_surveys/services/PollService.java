ea7ac9487964ad2fc4846d5493eb0d87cda0d8cd
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-20 12:22:58 +0300

    #30 — removed redundant for now PollService functions

--- src/main/java/epolsoft/practice/smart_surveys/services/PollService.java
+++ src/main/java/epolsoft/practice/smart_surveys/services/PollService.java
@@ -1,16 +1,10 @@
 package epolsoft.practice.smart_surveys.services;
 
 import epolsoft.practice.smart_surveys.entity.Poll;
-import epolsoft.practice.smart_surveys.exceptions.NotFoundException;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface PollService {
-    void createPoll(Poll poll);
-
     Poll getPollById(Long pollId);
 
-    void checkById(Long id) throws NotFoundException;
-
-    void updatePoll(Poll poll, Long pollId);
-
-    void deletePoll(Long pollId);
+    void checkById(Long id) throws ResponseStatusException;
 }
