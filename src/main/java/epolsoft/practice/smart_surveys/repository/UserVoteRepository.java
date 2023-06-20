edd472a0317e368b2af3799e8cc6755c73d4dad8
Cu-Rent <gray.xavier@mail.ru>
2023-06-20 16:20:59 +0300

    #32 - убрал обновление параметра vote_count

--- src/main/java/epolsoft/practice/smart_surveys/repository/UserVoteRepository.java
+++ src/main/java/epolsoft/practice/smart_surveys/repository/UserVoteRepository.java
@@ -6,9 +6,4 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserVoteRepository extends JpaRepository<UserVote, Long> {
-
-    @Modifying
-    @Query("update AnswerOption ao set ao.votedCount = ao.votedCount + 1  where ao.id = ?1")
-    void incrementVoteCount(@Param("answerOptionId") Long answerOptionId);
-}
+public interface UserVoteRepository extends JpaRepository<UserVote, Long> { }
