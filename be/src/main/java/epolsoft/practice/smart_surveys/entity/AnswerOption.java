d4e36fa5d11ce84bb2460324855331c7af4a820a
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-20 14:39:47 +0300

    Changed Poll_type to PollType (camelCase) + changed entites generation strategy to IDENTITY

--- be/src/main/java/epolsoft/practice/smart_surveys/entity/AnswerOption.java
+++ be/src/main/java/epolsoft/practice/smart_surveys/entity/AnswerOption.java
@@ -18,7 +18,7 @@ import lombok.Data;
 public class AnswerOption
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JsonIgnore
