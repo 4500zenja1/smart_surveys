cbab93c537de20f496b4dfaaff4b2e57753f4403
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-20 18:06:19 +0300

    Fixed bugs + inserted parental entities now

--- be/src/main/java/epolsoft/practice/smart_surveys/services/impl/PollServiceImpl.java
+++ be/src/main/java/epolsoft/practice/smart_surveys/services/impl/PollServiceImpl.java
@@ -2,10 +2,10 @@ package epolsoft.practice.smart_surveys.services.impl;
 
 import epolsoft.practice.smart_surveys.entity.AnswerOption;
 import epolsoft.practice.smart_surveys.entity.Poll;
-import epolsoft.practice.smart_surveys.exceptions.NotFoundException;
 import epolsoft.practice.smart_surveys.repository.PollRepository;
 import epolsoft.practice.smart_surveys.services.AnswerOptionService;
 import epolsoft.practice.smart_surveys.services.PollService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +27,7 @@ public class PollServiceImpl implements PollService {
     public Poll createPoll(Poll poll) {
         List<AnswerOption> answers = poll.getAnswers();
         for (AnswerOption answerOption: answers) {
+            answerOption.setPoll(poll);
             answerOptionService.createAnswerOption(answerOption);
         }
         return pollRepository.save(poll);
