1e77bd567e2dd7adea00b6dcd8c788b2663c990a
Cu-Rent <gray.xavier@mail.ru>
2023-06-20 14:05:47 +0300

    #32 - создал Response dto для UserVote

---
+++ src/main/java/epolsoft/practice/smart_surveys/dto/UserVoteResponseDto.java
@@ -0,0 +1,19 @@
+package epolsoft.practice.smart_surveys.dto;
+
+import epolsoft.practice.smart_surveys.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserVoteResponseDto {
+    private Long id;
+
+    private Long answerOptionId;
+
+    private Long userId;
+
+    private String text;
+}
