0b13f3290ee6d98ec470279482c289d6b6ec11c4
Cu-Rent <gray.xavier@mail.ru>
2023-06-20 14:08:45 +0300

    #32 - перенес логику работы контроллера из UserVoteController в SurveyController

--- src/main/java/epolsoft/practice/smart_surveys/controller/SurveyController.java
+++ src/main/java/epolsoft/practice/smart_surveys/controller/SurveyController.java
@@ -2,19 +2,21 @@ package epolsoft.practice.smart_surveys.controller;
 
 import epolsoft.practice.smart_surveys.dto.AccessSurveyResponseDto;
 import epolsoft.practice.smart_surveys.dto.SurveyResponseDto;
+import epolsoft.practice.smart_surveys.dto.UserVoteRequestDto;
+import epolsoft.practice.smart_surveys.dto.UserVoteResponseDto;
 import epolsoft.practice.smart_surveys.entity.AccessSurvey;
 import epolsoft.practice.smart_surveys.entity.Survey;
+import epolsoft.practice.smart_surveys.entity.UserVote;
 import epolsoft.practice.smart_surveys.mapper.AccessSurveyMapper;
 import epolsoft.practice.smart_surveys.mapper.SurveyMapper;
+import epolsoft.practice.smart_surveys.mapper.UserVoteMapper;
 import epolsoft.practice.smart_surveys.services.SurveyService;
+import epolsoft.practice.smart_surveys.services.UserVoteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,10 +29,17 @@ public class SurveyController {
     private SurveyService surveyService;
 
     @Autowired
+    private UserVoteService userVoteService;
+
+    @Autowired
     private AccessSurveyMapper accessSurveyMapper;
 
     @Autowired
     private SurveyMapper surveyMapper;
+
+    @Autowired
+    private UserVoteMapper userVoteMapper;
+
 
     @Operation(summary = "Получить опрос по id")
     @GetMapping("/{id}")
@@ -51,5 +60,20 @@ public class SurveyController {
     public List<AccessSurveyResponseDto> getAccessSurveys(@PathVariable Long id){
         List<AccessSurvey> accessSurveys = surveyService.getAllAccessSurveysByUserId(id);
         return accessSurveyMapper.accessSurveysToAccessSurveyResponseDto(accessSurveys);
+    }
+
+
+
+
+    @Operation(summary = "записать результаты опроса в бд")
+    @PostMapping("/{id}/submit")
+    public List<UserVoteResponseDto> setUserVote(
+            @PathVariable Long id,
+            @RequestBody List<UserVoteRequestDto> userVoteDtos,
+            @RequestParam(value = "user_id") Long user_id) {
+        for (UserVoteRequestDto userVoteDto : userVoteDtos) userVoteDto.setUserId(user_id);
+        System.out.println(userVoteDtos);
+        List<UserVote> userVotes = userVoteMapper.toEntity(userVoteDtos);
+        return userVoteMapper.toResponseDtos(userVoteService.createUserVotes(userVotes));
     }
 }
