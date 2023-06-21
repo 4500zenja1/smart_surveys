3c254f0cf9dd97b4f1b59b055f2efff0d1def806
Cu-Rent <gray.xavier@mail.ru>
2023-06-21 12:54:02 +0300

    #32 - убрал лишний id

--- src/main/java/epolsoft/practice/smart_surveys/dto/UserVoteResponseDto.java
+++ src/main/java/epolsoft/practice/smart_surveys/dto/UserVoteResponseDto.java
@@ -9,8 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserVoteResponseDto {
-    private Long id;
-
     private Long answerOptionId;
 
     private Long userId;
