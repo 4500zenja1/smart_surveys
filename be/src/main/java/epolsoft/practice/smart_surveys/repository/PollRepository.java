59de8d5337e58a38fdf86b0fc0ede4f93c392e63
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-21 15:38:48 +0300

    managing the branch to the BE/FE period

--- be/src/main/java/epolsoft/practice/smart_surveys/repository/PollRepository.java
+++ be/src/main/java/epolsoft/practice/smart_surveys/repository/PollRepository.java
@@ -2,6 +2,8 @@ package epolsoft.practice.smart_surveys.repository;
 
 import epolsoft.practice.smart_surveys.entity.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PollRepository extends JpaRepository<Poll, Long> {
 }
