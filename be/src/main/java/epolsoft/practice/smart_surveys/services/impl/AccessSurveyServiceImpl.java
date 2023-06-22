e52d9fd28097030068ed4676b3601747c3663166
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2023-06-22 11:14:07 +0300

    Added accessSurvey creation

--- be/src/main/java/epolsoft/practice/smart_surveys/services/impl/AccessSurveyServiceImpl.java
+++ be/src/main/java/epolsoft/practice/smart_surveys/services/impl/AccessSurveyServiceImpl.java
@@ -1,8 +1,13 @@
 package epolsoft.practice.smart_surveys.services.impl;
 
+import epolsoft.practice.smart_surveys.dto.AccessSurveyRequestDto;
 import epolsoft.practice.smart_surveys.entity.AccessSurvey;
+import epolsoft.practice.smart_surveys.entity.Survey;
+import epolsoft.practice.smart_surveys.entity.User;
+import epolsoft.practice.smart_surveys.mapper.AccessSurveyMapper;
 import epolsoft.practice.smart_surveys.repository.AccessSurveyRepository;
 import epolsoft.practice.smart_surveys.services.AccessSurveyService;
+import epolsoft.practice.smart_surveys.services.SurveyService;
 import epolsoft.practice.smart_surveys.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +24,29 @@ public class AccessSurveyServiceImpl implements AccessSurveyService {
     private AccessSurveyRepository accessSurveyRepository;
 
     @Autowired
+    private AccessSurveyMapper accessSurveyMapper;
+
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private SurveyService surveyService;
+
+    @Override
+    @Transactional
+    public AccessSurvey createAccessSurvey(AccessSurveyRequestDto accessSurveyRequestDto) {
+        AccessSurvey accessSurvey = accessSurveyMapper.toEntity(accessSurveyRequestDto);
+
+        Long surveyId = accessSurveyRequestDto.getSurveyId();
+        Survey survey = surveyService.getSurveyById(surveyId);
+        accessSurvey.setSurvey(survey);
+
+        Long userId = accessSurveyRequestDto.getUserId();
+        User user = userService.getUserById(userId);
+        accessSurvey.setUser(user);
+
+        return accessSurveyRepository.save(accessSurvey);
+    }
 
     @Override
     @Transactional(readOnly = true)
