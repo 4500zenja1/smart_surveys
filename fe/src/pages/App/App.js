5e1842aef58bd75c97264036e2cbf7cafa82a6da
Nikita Kozel <niknikkzel@gmail.com>
2023-06-27 15:20:45 +0300

    #46 clear unusable modules

--- fe/src/pages/App/App.js
+++ fe/src/pages/App/App.js
@@ -1,14 +1,8 @@
-import React, {Component} from 'react';
-import { Menu, Avatar, Layout } from 'antd';
+import React from 'react';
+import { Layout } from 'antd';
 //import { Header } from '../components/header.js';
-import { BrowserRouter as Router, Route, Routes, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login, Survey, SurveyList, UsersManager, PersonalAccount} from '../index.js'
-
-const Somecomponent = () =>{
-    return (
-        <div>hi</div>
-    )
-}
 
 function App () {
         return (
