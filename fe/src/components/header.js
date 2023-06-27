4d3916688532d119f14c5333f53e59691ae07943
Nikita Kozel <niknikkzel@gmail.com>
2023-06-27 04:27:33 +0300

    #53 create Header component

---
+++ fe/src/components/header.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { Menu, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+
+let name = "Segey Berr"
+
+const items = [
+    {
+        label: (   
+            <NavLink to="/home" >Smart Surveys</NavLink>            
+        ),
+        key: 'smartsurveys',
+
+    },
+    {
+        label: (
+            <NavLink to="/home" >Home</NavLink>
+        ),
+        key: 'home'
+    },
+    {
+        label: (
+            <NavLink to="/user/id:">
+                <Avatar size="small" icon={<UserOutlined />}/>
+                <span>{name}</span>
+            </NavLink>
+        ),
+        key: 'avatar'
+    }
+]
+
+class Header extends Component {
+    render () {
+        return (
+                <Router>
+                    <Menu theme = "dark" mode = "horizontal" items={items}/>
+                </Router>
+                // <header style = {{display: "inline"}}>
+                //     <h1 style = {{display: "inline"}}>Smart Surveys</h1>
+                //     <h2 style = {{display: "inline"}}>Home</h2>
+                //     <div >
+                //         <Avatar ></Avatar>
+                //         <span>{name}</span>
+                //     </div>
+                // </header>
+    )}
+}
+
+export {Header}
--- fe/src/pages/index.js
+++ fe/src/pages/index.js
@@ -1,1 +1,16 @@
-//.gitkeep))
+import React, {Component} from 'react';
+import { Menu, Avatar, Layout } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Header } from '../components/header';
+
+
+class App extends Component {
+    render () {
+        return (
+                <Layout>
+                    <Header></Header>
+                </Layout>
+    )}
+}
+
+export default App
