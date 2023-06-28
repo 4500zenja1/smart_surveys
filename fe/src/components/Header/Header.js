a41dcea0623112414a6b43fffb00912c1b83b808
Nikita Kozel <niknikkzel@gmail.com>
2023-06-28 12:52:49 +0300

    #53 remove unnecessary modules

--- fe/src/components/Header/Header.js
+++ fe/src/components/Header/Header.js
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Menu } from 'antd';
 import { items } from './Header.config.js'
-import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { NavLink } from "react-router-dom";
-
 let name ="Sergey Berr"
 
 function Header() {
