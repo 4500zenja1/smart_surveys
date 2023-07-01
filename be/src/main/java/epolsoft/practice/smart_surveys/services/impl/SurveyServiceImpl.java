fa01056a7d8ac0a1a7f7d0bb373dcaa20f0d5ee9
Cu-Rent <gray.xavier@mail.ru>
2023-07-02 00:50:28 +0300

    #39 - перенес логику создания отчета в отдельный сервис

--- be/src/main/java/epolsoft/practice/smart_surveys/services/impl/SurveyServiceImpl.java
+++ be/src/main/java/epolsoft/practice/smart_surveys/services/impl/SurveyServiceImpl.java
@@ -1,13 +1,5 @@
 package epolsoft.practice.smart_surveys.services.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-import epolsoft.practice.smart_surveys.dto.AnswerOptionResponseDto;
 import epolsoft.practice.smart_surveys.dto.PollRequestDto;
 import epolsoft.practice.smart_surveys.dto.SurveyRequestDto;
 import epolsoft.practice.smart_surveys.entity.AnswerOption;
@@ -18,7 +10,6 @@ import epolsoft.practice.smart_surveys.entity.UserVote;
 import epolsoft.practice.smart_surveys.entity.enums.AnswerType;
 import epolsoft.practice.smart_surveys.exceptions.NotFoundException;
 import epolsoft.practice.smart_surveys.exceptions.ValidationException;
-import epolsoft.practice.smart_surveys.mapper.AnswerOptionMapper;
 import epolsoft.practice.smart_surveys.mapper.SurveyMapper;
 import epolsoft.practice.smart_surveys.repository.SurveyRepository;
 import epolsoft.practice.smart_surveys.services.AccessSurveyService;
@@ -31,14 +22,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
+
 
 @Service
 @RequiredArgsConstructor
@@ -64,9 +51,6 @@ public class SurveyServiceImpl implements SurveyService {
 
     @Autowired
     private UserVoteService userVoteService;
-
-    @Autowired
-    private AnswerOptionMapper answerOptionMapper;
 
     @Override
     @Transactional
@@ -169,48 +153,5 @@ public class SurveyServiceImpl implements SurveyService {
         if (!surveyRepository.existsById(id)) {
             throw new NotFoundException("Не найден опрос с таким id в базе данных");
         }
-    }
-
-    @Override
-    public Document getReport(Long id, String path, Survey survey) throws FileNotFoundException, DocumentException, JsonProcessingException {
-        checkById(id);
-        String FILE;
-
-        Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);//заголовок опроса
-        Font answerFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);//ответ
-        Font descriptionFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.NORMAL);//описание
-        Font questionFont = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.BOLD);//вопрос
-
-        ObjectMapper mapper = new ObjectMapper();
-        Map map = mapper.readValue(path, Map.class);
-        String file_path = map.get("path").toString();
-
-        if(file_path.isEmpty())  FILE = "d:/surveyReport_" + id + ".pdf";
-        else FILE = file_path;
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream(FILE));
-        document.open();
-
-        Paragraph preface = new Paragraph();
-        preface.add(new Paragraph(survey.getSurveyTitle(), titleFont));
-        preface.add(new Paragraph(" "));
-        preface.add(new Paragraph(survey.getSurveyDescription(), descriptionFont));
-        preface.add(new Paragraph(" "));
-
-        List<Poll> polls = survey.getPolls();
-        for (Poll poll : polls) {
-            List<AnswerOption> answers = poll.getAnswers();
-            List<AnswerOptionResponseDto> answersDto = answerOptionMapper.toResponseDtos(answers);
-            preface.add(new Paragraph(poll.getQuestion(), questionFont));
-            for (AnswerOptionResponseDto answer : answersDto) {
-                preface.add(new Paragraph(answer.getOption() +
-                        " - " + Math.round(answer.getVotedInPercent()) + "% " +
-                        "(" + answer.getVotedCount() + ")", answerFont));
-            }
-            preface.add(new Paragraph(" "));
-        }
-        document.add(preface);
-        document.close();
-        return document;
     }
 }
